8c297cfd2b80e96518f963a7d2b65263294c9565
Marc Egli <frog32@me.com>
2010-12-09 17:01:43 +0100

    corrected production settings

--- guckstdu/settings_production.py
+++ guckstdu/settings_production.py
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db_guckstdu_production',                      # Or path to database file if using sqlite3.
+        'USER': 'db',                      # Not used with sqlite3.
+        'PASSWORD': '****',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }

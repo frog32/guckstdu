f1e99af6e2a3d1d69436edd65adc5c27576298ff
Tarik <Tarik@Machine.(none)>
2010-12-19 15:38:00 +0100

    series, season, episode view

--- guckstdu/series/urls.py
+++ guckstdu/series/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^top/$', 'series.views.top_series', name='top_series'),
+    url(r'^series/$', 'series.views.series_details', name='series_details'),
+    url(r'^season/$', 'series.views.season_details', name='season_details'),
+    url(r'^episode/$', 'series.views.episode_details', name='episode_details'),
 )

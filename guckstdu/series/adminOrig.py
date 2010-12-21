aaf625ff81ae340a90d68d95961532559b374e30
sandor <sandor@kan.com>
2010-12-21 20:04:44 +0100

    saved original admin.py for backup

---
+++ guckstdu/series/adminOrig.py
@@ -0,0 +1,37 @@
+from series.models import Series, Season, Episode, Station, Runtime
+
+from django.conf.urls.defaults import patterns, url, include
+from django.contrib import admin
+
+class EpisodeInline(admin.TabularInline):
+    model = Episode
+    extra = 0
+
+class SeasonAdmin(admin.ModelAdmin):
+    inlines = (EpisodeInline,)
+    list_display = ('__unicode__', 'episode_count')
+    # list_filter = ('series',)
+
+    def changelist_view(self, *args, **kwargs):
+        return super(SeasonAdmin, self).changelist_view(*args, **kwargs)
+
+class SeasonInline(admin.TabularInline):
+    model = Season
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__',)
+    
+    def get_urls(self):
+        urlpatterns = super(SeriesAdmin, self).get_urls()
+        season_admin = SeasonAdmin(Season,self.admin_site)
+        extra_urlpatterns = patterns('',
+                            url(r'^(.+)/season/',include(season_admin.urls)),
+                        )
+        return extra_urlpatterns + urlpatterns
+
+class EpisodeAdmin(admin.ModelAdmin):
+    pass
+    
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Season, SeasonAdmin)
+admin.site.register(Episode, EpisodeAdmin)
